b816ba8d3f29c4ee8f557070f9768fd56712ab72
Matheus de Castro <matheusdecastroviana@gmail.com>
2023-09-19 17:00:49 -0300

    Criação da aura atrás da tabela
    Criei a aura atrás da tabela, adicionei a imagem back e o menu hamburguer.

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuHamburguer = document.querySelector('.menuHamburguer'); 
+const menu = document.querySelector('.menu'); 
+
+menuHamburguer.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
