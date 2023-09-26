396bf8c5544783394c6d31eaf40747175f2fef12
Matheus de Castro <matheusdecastroviana@gmail.com>
2023-09-26 16:54:07 -0300

    Criação do footer e do selector
    Criei o footer e o selector.

--- script1.js
+++ script1.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menuHamburguer = document.querySelector('.menuHamburguer');
     const menuResponsive = document.querySelector('.menuResponsive');
+    const menuPersonagem = document.getElementById('personagem');
+    const selectedPersonagemImg = document.getElementById('selected-personagem-img');
+    const selectedPersonagemText = document.getElementById('selected-personagem-text');
+    let pontuacao = 0;
 
     menuHamburguer.addEventListener('click', () => {
         menuResponsive.classList.toggle('active');
@@ -12,9 +16,30 @@ document.addEventListener("DOMContentLoaded", function () {
         menuResponsive.classList.remove('active');
     });
 
-    let pontuacao = 0;
-    pontuacao += 10; //Aumenta a pontuação em 10
+    // Atualiza a imagem e o texto do personagem selecionado
+    menuPersonagem.addEventListener('change', function () {
+        const personagemSelecionado = menuPersonagem.value;
+        const personagemOption = menuPersonagem.querySelector(`[value="${personagemSelecionado}"]`);
+        const personagemImgSrc = personagemOption.querySelector('img').getAttribute('src');
+        const personagemNome = personagemOption.textContent;
 
-    //Atualiza o elemento HTML com a nova pontuação.
-    document.getElementById("pontuacao").textContent = pontuacao;
+        selectedPersonagemImg.src = personagemImgSrc;
+        selectedPersonagemText.textContent = personagemNome;
+
+        // Lógica para alterar a pontuação quando um personagem é selecionado
+        if (personagemSelecionado === "personagem1") {
+            pontuacao += 10; // Exemplo de aumento de pontuação para Midoriya
+        } else if (personagemSelecionado === "personagem2") {
+            pontuacao += 15; // Exemplo de aumento de pontuação para Bakugo
+        } else {
+            pontuacao += 5; // Pontuação padrão para outros personagens
+        }
+
+        // Atualiza o elemento HTML com a nova pontuação.
+        pontuacaoElement.textContent = pontuacao;
+    });
+
+    // Inicialmente, defina a pontuação inicial
+    const pontuacaoElement = document.getElementById('pontuacao');
+    pontuacaoElement.textContent = pontuacao;
 });
