c127a71becc653ad07e38c4a71779773f09c21f9
Matheus de Castro <matheusdecastroviana@gmail.com>
2023-11-16 16:55:09 -0300

    Começo da criação do jogo da velha
    Criei o começo do jogo da velha, já estando funcional, mas ainda não estilizado.

--- script1.js
+++ script1.js
@@ -58,3 +58,82 @@ document.addEventListener("DOMContentLoaded", function () {
     scriptLoadedMarker.id = 'script1-loaded';
     document.body.appendChild(scriptLoadedMarker);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cells = document.querySelectorAll('#game-board td');
+    const resetButton = document.getElementById('reset-button');
+    const placarTimeAzul = document.getElementById('placar-time-azul');
+    const placarTimeVermelho = document.getElementById('placar-time-vermelho');
+
+    let currentPlayer = 'X';
+    let gameActive = true;
+    let moves = 0;
+
+    // evento de clique para cada célula do tabuleiro
+    cells.forEach(cell => {
+        cell.addEventListener('click', handleCellClick);
+    });
+
+    // evento de clique para o botão de reiniciar o jogo
+    resetButton.addEventListener('click', resetGame);
+
+    function handleCellClick() {
+        // Verificação se a célula já foi marcada e se o jogo ainda está ativo
+        if (this.textContent || !gameActive) {
+            return;
+        }
+
+        // Marcando a célula com o símbolo do jogador atual
+        this.textContent = currentPlayer;
+
+        // Verificando se o jogador atual venceu
+        if (checkWin()) {
+            updatePlacar();
+            resetGame();
+            return;
+        }
+
+        // Verificando se houve empate
+        moves++;
+        if (moves === 9) {
+            resetGame();
+            return;
+        }
+
+        // Alternando para o próximo jogador
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+
+    function checkWin() {
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Linhas
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas
+            [0, 4, 8], [2, 4, 6]             // Diagonais
+        ];
+
+        return winningCombinations.some(combination => {
+            const [a, b, c] = combination;
+            return cells[a].textContent && cells[a].textContent === cells[b].textContent && cells[a].textContent === cells[c].textContent;
+        });
+    }
+
+    function updatePlacar() {
+        if (currentPlayer === 'X') {
+            placarTimeAzul.textContent = parseInt(placarTimeAzul.textContent) + 1;
+        } else {
+            placarTimeVermelho.textContent = parseInt(placarTimeVermelho.textContent) + 1;
+        }
+    }
+
+    function resetGame() {
+        // Limpando o conteúdo das células
+        cells.forEach(cell => {
+            cell.textContent = '';
+        });
+
+        // Reiniciar as variáveis
+        currentPlayer = 'X';
+        gameActive = true;
+        moves = 0;
+    }
+});
