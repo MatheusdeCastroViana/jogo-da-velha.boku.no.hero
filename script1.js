d920051b64353ea0f27238a099759cca1da42f35
Matheus de Castro <matheusdecastroviana@gmail.com>
2023-11-18 11:19:00 -0300

    Contra o Computador!
    Inseri a opção de jogar contra o computador.

--- script1.js
+++ script1.js
@@ -1,67 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Seletor do menu hamburguer
     const menuHamburguer = document.querySelector('.menuHamburguer');
+    // Seletor do menu responsivo
     const menuResponsive = document.querySelector('.menuResponsive');
+    // Seletor do menu de seleção de personagens
     const customSelect = document.getElementById('personagem');
+    // Opções do menu de seleção de personagens
     const options = customSelect.querySelectorAll('.options li');
+    // Elementos dos personagens e vilões
     const personagens = document.querySelectorAll('.personagem');
     const viloes = document.querySelectorAll('.vilao');
+
+    // Pontuação do time azul
     const pontuacaoElement = document.getElementById('placar-time-azul');
     let pontuacao = 0;
 
+    // Evento de clique para cada opção do menu de seleção de personagens
     options.forEach(option => {
-        option.addEventListener('click', function () {
-            const value = this.getAttribute('data-value');
-            const imgSrc = this.querySelector('img').getAttribute('src');
-
-            selectedPersonagemImg.src = imgSrc;
-            selectedPersonagemText.textContent = value;
-            customSelect.classList.remove('open');
-
-            // Exibir ou ocultar heróis e vilões conforme a seleção
-            if (value === 'Heróis') {
-                personagens.forEach(personagem => personagem.style.display = 'block');
-                viloes.forEach(vilao => vilao.style.display = 'none');
-            } else if (value === 'Vilões') {
-                personagens.forEach(personagem => personagem.style.display = 'none');
-                viloes.forEach(vilao => vilao.style.display = 'block');
-            }
-        });
+        option.addEventListener('click', handleCharacterSelection);
     });
 
-    menuHamburguer.addEventListener('click', () => {
-        menuResponsive.classList.toggle('active');
-    });
+    // Evento de clique para o menu hamburguer
+    menuHamburguer.addEventListener('click', toggleResponsiveMenu);
 
+    // Fechar o menu responsivo
     const closeMenuButton = document.getElementById('closeMenu');
+    closeMenuButton.addEventListener('click', closeResponsiveMenu);
 
-    closeMenuButton.addEventListener('click', () => {
-        menuResponsive.classList.remove('active');
-    });
+    // Evento de clique para o menu de seleção de personagens
+    customSelect.addEventListener('click', toggleCharacterMenu);
 
-    options.forEach(option => {
-        option.addEventListener('click', function () {
-            const value = this.getAttribute('data-value');
-            const imgSrc = this.getAttribute('data-img');
-
-            selectedPersonagemImg.src = imgSrc;
-            selectedPersonagemText.textContent = value;
-            customSelect.classList.remove('open');
-        });
-    });
-
-    customSelect.addEventListener('click', function () {
-        // Alternar a abertura e o fechamento do menu de opções
-        this.classList.toggle('open');
-    });
-
+    // Marcador para o script carregado
     const scriptLoadedMarker = document.createElement('div');
     scriptLoadedMarker.id = 'script1-loaded';
     document.body.appendChild(scriptLoadedMarker);
-});
 
-document.addEventListener("DOMContentLoaded", function () {
+    // Elementos do tabuleiro do jogo
     const cells = document.querySelectorAll('#game-board td');
+    // Botão de reiniciar o jogo
     const resetButton = document.getElementById('reset-button');
+    // Elementos do placar
     const placarTimeAzul = document.getElementById('placar-time-azul');
     const placarTimeVermelho = document.getElementById('placar-time-vermelho');
 
@@ -69,46 +47,66 @@ document.addEventListener("DOMContentLoaded", function () {
     let gameActive = true;
     let moves = 0;
 
-    // evento de clique para cada célula do tabuleiro
+    // Adicionar evento de clique para cada célula do tabuleiro
     cells.forEach(cell => {
         cell.addEventListener('click', handleCellClick);
     });
 
-    // evento de clique para o botão de reiniciar o jogo
+    // Adicionar evento de clique para o botão de reiniciar o jogo
     resetButton.addEventListener('click', resetGame);
 
+    // Função para lidar com a seleção de personagens
+    function handleCharacterSelection() {
+        const value = this.getAttribute('data-value');
+        const imgSrc = this.querySelector('img').getAttribute('src');
+
+        selectedPersonagemImg.src = imgSrc;
+        selectedPersonagemText.textContent = value;
+        customSelect.classList.remove('open');
+
+        // Exibir ou ocultar heróis e vilões conforme a seleção
+        if (value === 'Heróis') {
+            personagens.forEach(personagem => personagem.style.display = 'block');
+            viloes.forEach(vilao => vilao.style.display = 'none');
+        } else if (value === 'Vilões') {
+            personagens.forEach(personagem => personagem.style.display = 'none');
+            viloes.forEach(vilao => vilao.style.display = 'block');
+        }
+    }
+
+    // Função para lidar com o clique no tabuleiro
     function handleCellClick() {
-        // Verificação se a célula já foi marcada e se o jogo ainda está ativo
         if (this.textContent || !gameActive) {
             return;
         }
 
-        // Marcando a célula com o símbolo do jogador atual
         this.textContent = currentPlayer;
 
-        // Verificando se o jogador atual venceu
         if (checkWin()) {
             updatePlacar();
             resetGame();
             return;
         }
 
-        // Verificando se houve empate
         moves++;
         if (moves === 9) {
             resetGame();
             return;
         }
 
-        // Alternando para o próximo jogador
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+
+        if (gameActive && currentPlayer === 'O') {
+            makeComputerMove();
+        }
     }
 
+    // Função para verificar se há uma vitória
     function checkWin() {
         const winningCombinations = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Linhas
-            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas
-            [0, 4, 8], [2, 4, 6]             // Diagonais
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
         ];
 
         return winningCombinations.some(combination => {
@@ -117,6 +115,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    // Função para atualizar o placar
     function updatePlacar() {
         if (currentPlayer === 'X') {
             placarTimeAzul.textContent = parseInt(placarTimeAzul.textContent) + 1;
@@ -125,15 +124,56 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    // Função para reiniciar o jogo
     function resetGame() {
-        // Limpando o conteúdo das células
         cells.forEach(cell => {
             cell.textContent = '';
         });
 
-        // Reiniciar as variáveis
         currentPlayer = 'X';
         gameActive = true;
         moves = 0;
     }
+
+    // Função para fazer a jogada do computador
+    function makeComputerMove() {
+        const emptyCells = Array.from(cells).filter(cell => !cell.textContent);
+        if (emptyCells.length > 0) {
+            const randomIndex = Math.floor(Math.random() * emptyCells.length);
+            const randomCell = emptyCells[randomIndex];
+            randomCell.textContent = 'O';
+
+            if (checkWin()) {
+                updatePlacar();
+                resetGame();
+                return;
+            }
+
+            moves++;
+            if (moves === 9) {
+                resetGame();
+                return;
+            }
+
+            currentPlayer = 'X';
+        }
+    }
 });
+
+// Função para alternar o menu responsivo
+function toggleResponsiveMenu() {
+    const menuResponsive = document.querySelector('.menuResponsive');
+    menuResponsive.classList.toggle('active');
+}
+
+// Função para fechar o menu responsivo
+function closeResponsiveMenu() {
+    const menuResponsive = document.querySelector('.menuResponsive');
+    menuResponsive.classList.remove('active');
+}
+
+// Função para alternar o menu de seleção de personagens
+function toggleCharacterMenu() {
+    const customSelect = document.getElementById('personagem');
+    customSelect.classList.toggle('open');
+}
